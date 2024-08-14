d63d3ab0c21f84286157d6a8ecedeec50c58ff84
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-08-14 01:32:04 -0300

    Ruleta y Cards implementadas

---
+++ js/cards.js
@@ -0,0 +1,37 @@
+let questions = ["¿Qué te motivo a dar mentorias a chicos de la fundación? ",
+    "¿Como conociste Pescar?", 
+    "¿Te habría gustado conocer Pescar antes de tu inicio laboral? ¿En que te habria ayudado?",
+    "¿Cual fue el mayor desafio que enfrentaste en tu trabajo?",
+    "¿Preferis el trabajo remoto o en oficina? ¿Por que?."
+];
+const container = document.querySelector('.card-container');
+let i = 0;
+questions.forEach(question => {
+    i++
+    let card = `<div class="card col-md-1">
+                <div class="face front">
+                    <img src="/cards/J.P. Morgan.png" alt="">
+                    <h3>Pregunta ${i}</h3>
+                </div>
+                <div class="face back">
+                    <h3>Pregunta ${i}</h3>
+                    <p>${question}</p>
+                    <div class="link">
+                        <a href="">Fundacion Pescar</a>
+                    </div>
+                </div>
+            </div>`;
+    container.innerHTML += card;
+    console.log(i);
+});
+
+const cards = document.querySelectorAll('.card');
+cards.forEach(card => {
+    card.addEventListener('click', () => 
+        {
+            card.style.transform = 'perspective(600px) rotateY(360deg)';
+            card.childNodes[3].style.transform = 'perspective(600px) rotateY(360deg)';                             
+        })    
+});
+
+
--- js/index.js
+++ js/index.js
@@ -1,18 +1,105 @@
 
 
-let numeroPreguntas = 10;
-let preguntas = [
-    '¿Cuáles son los beneficios de la Multimedia Visual? ',
-    '¿Qué es la multimedia visual?',
-    'La imagen multimedia se trata de la posibilidad del lenguaje verbal más otros lenguajes gráficos en la pantalla del ordenador. Se ubica dentro de las imágenes proyectadas en movimiento. Verdadero o falso',
-    '¿Qué tiempo de imágenes existen en la multimedia visual?',
-    'La multimedia visual sólo se basa en imágenes estáticas. Verdadero o Falso',
-    '¿Qué elementos hay que tener en cuenta en la multimedia visual?',
-    'Los podcast y los temas que tienen audio igual cuentan en la multimedia visual. Verdadero o Falso',
-    'Tipo de información que es la multimedia visual',
-    'La fase de conceptualización y diseño implica en el entorno multimedia. Verdadero o Falso',
-    '¿Qué recursos son necesarios en la multimedia visual?'
+let numeroPreguntas = 20;
+let num = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
 ]
+let mentores = [
+    "Alan Nahuel, P.",
+    "Alejandro Rodrigo, D.",
+    "Alfredo Eduardo, A.",
+    "Alma Delia, V. C. ",
+    "Anahi Soledad, G.",
+    "Angeles, R.",
+    "Argenis Eduardo, M. T.",
+    "Ariadna, N. ",
+    "Augusto Matias, A.",
+    "Brisa Ayelen, Y.",
+    "Bruno Agustin, G.",
+    "Camila Antonella, D.",
+    "Carlos Javier, M. S.",
+    "Daniela, P.",
+    "Diego Ernesto, V.",
+    "Diego Gerardo, F.",
+    "Diego German, S.",
+    "Diego Leandro, P.",
+    "Diego Sebastian, S.",
+    "Eduardo Ignacio, G.",
+]
+let mentores2 = [
+    "Ezequiel, C.",
+    "Ezequiel Edgardo, V.",
+    "Federico, F.",
+    "Federico O, O.",
+    "Florencia Ayelen, N.",
+    "Florencia Camila, M.",
+    "Florencia Laura, M.",
+    "Florencia, W.",
+    "Gabriel Augusto, C. de A.",
+    "Geraldine, D.",
+    "Gina, F.",
+    "Guido Alejandro, A.",
+    "Ignacio David, B.",
+    "Javier, F.",
+    "Jimena Florencia, M.",
+    "Joaquin, G.",
+    "Joaquin S, S.",
+    "Johansen Dennys, F. C. ",
+    "Jose Gabriel, L. ",
+    "Jose Maria Fernando, F. M.",
+    "Victoria Abril, B.",
+    "Segui participando!!!!!",
+]
+let mentoresConNumeros = {};
+for (let i = 0; i < 20; i++) {
+    mentoresConNumeros[mentores[i]] = i;
+}
+console.log(mentoresConNumeros)
+
+let mentoresConNumeros2 = {};
+for (let i = 0; i < 22; i++) {
+    mentoresConNumeros2[mentores2[i]] = i + 20;
+}
+console.log(mentoresConNumeros2)
 
 
 class Pregunta {
@@ -26,14 +113,14 @@ class Pregunta {
 
         let modeloPregunta = '';
 
-        modeloPregunta = modeloPregunta + `<div class="modal fade" id= myModal${this.numero}  tabindex="-1" role="dialog" aria-labelledby="myModalLabel"  style="width: 90%;margin: auto">`
+        modeloPregunta = modeloPregunta + `<div class="modal fade" id= ${this.numero}  tabindex="-1" role="dialog" aria-labelledby="myModalLabel"  style="width: 90%;margin: auto">`
         modeloPregunta = modeloPregunta + '    <div class="modal-dialog" role="document" style="width: 90%;margin: auto">'
         modeloPregunta = modeloPregunta + '        <div class="modal-content" style="width: 90%;margin: auto">'
         modeloPregunta = modeloPregunta + '            <div class="modal-header">'
         modeloPregunta = modeloPregunta + '                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'
         modeloPregunta = modeloPregunta + '                <center>'
         modeloPregunta = modeloPregunta + '                    <h2 class="modal-title" id="myModalLabel" style="color:#000">'
-        modeloPregunta = modeloPregunta + `                        PREGUNTA ${this.numero}</h2>`
+        modeloPregunta = modeloPregunta + `                        MENTOR</h2>`
         modeloPregunta = modeloPregunta + '            </div>'
         modeloPregunta = modeloPregunta + '            <div class="modal-body">'
         modeloPregunta = modeloPregunta + '                <br>'
@@ -46,7 +133,7 @@ class Pregunta {
         modeloPregunta = modeloPregunta + '        </div>'
         modeloPregunta = modeloPregunta + '        <br>'
         modeloPregunta = modeloPregunta + '        <div class="modal-footer">'
-        modeloPregunta = modeloPregunta + '            <button type="button" class="btn btn-danger" data-dismiss="modal">Salir</button>'
+        modeloPregunta = modeloPregunta + '            <button type="button" class="btn" data-dismiss="modal">Salir</button>'
         modeloPregunta = modeloPregunta + '        </div>'
         modeloPregunta = modeloPregunta + '    </div>'
         modeloPregunta = modeloPregunta + '  </div>'
@@ -64,40 +151,85 @@ const generarColor = () => {
     let simbolos = '0123456789ABCDEF';
     color = "#";
 
-	for(var i = 0; i < 6; i++){
-		color = color + simbolos[Math.floor(Math.random() * 16)];
-	}
+    for (var i = 0; i < 6; i++) {
+        color = color + simbolos[Math.floor(Math.random() * 16)];
+    }
     return color;
 }
 
 // Creas la rebanada de pizza
 const rebanadaPizza = () => {
     rebanadas = [];
-    for(i = 0; i < numeroPreguntas; i++ ){
-    
-        rebanadas[i] = {
-            'text': `Pregunta ${ i + 1}`,
-            'fillStyle' : `${generarColor()}`
+    for (i = 0; i < 20; i++) {
+        if (i % 2 == 0) {
+            rebanadas[i] = {
+                'text': `${mentores[i]}`,
+                'fillStyle': 'rgba(255, 177, 113)',
+                'textFontSize': 14,
+                'textFillStyle': 'rgba(84, 41, 15)',
+            }
         }
-       
+        else {
+            rebanadas[i] = {
+                'text': `${mentores[i]}`,
+                'fillStyle': 'rgba(84, 41, 15)',
+                'textFontSize': 14,
+                'textFillStyle': 'rgba(255, 177, 113)'
+            }
+        }
+    }
+    return rebanadas;
+}
+const rebanadaPizza2 = () => {
+    rebanadas = [];
+    for (i = 0; i < 22; i++) {
+        if (i % 2 == 0) {
+            rebanadas[i] = {
+                'text': `${mentores2[i]}`,
+                'fillStyle': 'rgba(255, 177, 113)',
+                'textFontSize': 14,
+                'textFillStyle': 'rgba(84, 41, 15)',
+            }
+        }
+        else {
+            rebanadas[i] = {
+                'text': `${mentores2[i]}`,
+                'fillStyle': 'rgba(84, 41, 15)',
+                'textFontSize': 14,
+                'textFillStyle': 'rgba(255, 177, 113)'
+            }
+        }
     }
     return rebanadas;
 }
 
+
 // creas toda la pizza
 let miRuleta = new Winwheel({
 
-    'numSegments' : numeroPreguntas,
-    'outerRadius' : 270,
+    'numSegments': numeroPreguntas,
+    'outerRadius': 270,
     'segments': rebanadaPizza(),
-    'animation':{
-        'type':'spinToStop',
+    'animation': {
+        'type': 'spinToStop',
         'duration': 6,
-        'callbackFinished':'Mensaje()',
-        'callbackAfter':'dibujarIndicador()'
+        'callbackFinished': 'Mensaje()',
+        'callbackAfter': 'dibujarIndicador()'
     }
 });
-
+let nuevaRuleta = new Winwheel(
+    {
+        'canvasId': 'canvas2',
+        'numSegments': numeroPreguntas + 2,
+        'outerRadius': 270,
+        'segments': rebanadaPizza2(),
+        'animation': {
+            'type': 'spinToStop',
+            'duration': 6,
+            'callbackFinished': 'Mensaje2()',
+            'callbackAfter': 'dibujarIndicador2()'
+        }
+    })
 
 
 // Dibujas el indicador
@@ -107,52 +239,78 @@ const dibujarIndicador = () => {
     ctx.fillStyle = 'black';
     ctx.lineWidth = 2;
     ctx.beginPath();
-    ctx.moveTo(300,0);
-    ctx.lineTo(320,0);
-    ctx.lineTo(300,40);
-    ctx.lineTo(280,0);
+    ctx.moveTo(300, 0);
+    ctx.lineTo(320, 0);
+    ctx.lineTo(300, 40);
+    ctx.lineTo(280, 0);
+    ctx.stroke();
+    ctx.fill();
+}
+const dibujarIndicador2 = () => {
+    var ctx = nuevaRuleta.ctx;
+    ctx.strokeStyle = 'navy';
+    ctx.fillStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(300, 0);
+    ctx.lineTo(320, 0);
+    ctx.lineTo(300, 40);
+    ctx.lineTo(280, 0);
     ctx.stroke();
     ctx.fill();
 }
 
 
 dibujarIndicador();
-
+dibujarIndicador2();
 
 
 // Crea el cuadro de la pregunta
 const Mensaje = () => {
     var SegmentoSeleccionado = miRuleta.getIndicatedSegment();
-    alert("Listo para la  " + SegmentoSeleccionado.text);
-
-    for(i = 0; i < numeroPreguntas; i++){
-        if(SegmentoSeleccionado.text == `Pregunta ${ i + 1 }`){
-            $( `#myModal${ i + 1}`).modal({backdrop:'static'});
-         
-        }
-    }
-    
-   
+    $(`#${mentoresConNumeros[SegmentoSeleccionado.text]}`).modal({ backdrop: 'static' });
     miRuleta.stopAnimation(false);
     miRuleta.rotationAngle = 0;
-    miRuleta.draw();
+    // miRuleta.draw();
+    dibujarIndicador();
+}
+const Mensaje2 = () => {
+    var SegmentoSeleccionado = nuevaRuleta.getIndicatedSegment();
+    console.log(SegmentoSeleccionado);
+    $(`#${mentoresConNumeros2[SegmentoSeleccionado.text]}`).modal({ backdrop: 'static' });
+    nuevaRuleta.stopAnimation(false);
+    nuevaRuleta.rotationAngle = 0;
+    // nuevaRuleta.draw();
     dibujarIndicador();
 }
 
 
 
-let preguntaTexto = document.createElement( 'div' );
-preguntaTexto.innerHTML = ''
-const pregunta = new Pregunta();
-for( i = 0; i < numeroPreguntas; i++){
+// let preguntaTexto = document.createElement('div');
 
-    pregunta.pregunta = preguntas[i];
-    pregunta.numero = i + 1;
-    
-    preguntaTexto.innerHTML = preguntaTexto.innerHTML + pregunta.modelo();
-    
+// const pregunta = new Pregunta();
+for (i = 0; i < numeroPreguntas; i++) {
+    let preguntaTexto = document.createElement('div');
+    preguntaTexto.innerHTML = "";
+    const pregunta = new Pregunta();
+    pregunta.pregunta = mentores[i];
+    pregunta.numero = i;
+    preguntaTexto.innerHTML = pregunta.modelo();
+    document.body.appendChild(preguntaTexto)
 }
-document.body.appendChild(preguntaTexto)
+for (i = 0; i < numeroPreguntas + 2; i++) {
+    let preguntaTexto = document.createElement('div');
+    preguntaTexto.innerHTML = "";
+    const pregunta = new Pregunta();
+    pregunta.pregunta = mentores2[i];
+    pregunta.numero = i + 20;
+    preguntaTexto.innerHTML = pregunta.modelo();
+    document.body.appendChild(preguntaTexto)
+}
+
+
+
+
 
 
 
